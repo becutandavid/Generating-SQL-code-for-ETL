d38b7ebd41417b38a92e5b1259022f47f598b13c
David Galevski <becutandavid@gmail.com>
2020-10-12 20:25:27 +0200

    email update

--- outputETL.sql
+++ outputETL.sql
@@ -1,45 +1,4 @@
 
- -- DDL for dim_course
-CREATE TABLE dim_course(
-  skey serial primary key,
-  id int4 NOT NULL,
-  name varchar NOT NULL,
-  description varchar,
-  start_date timestamp NOT NULL default(NOW()),
-  end_date timestamp default("9999-12-31")
-);
-
- -- Procedure for dim_course
-CREATE OR REPLACE PROCEDURE sp_performETL_Course
-LANGUAGE PLPGSQL
-AS $$
-BEGIN
-
-INSERT INTO dim_course
-SELECT platforma.courses.id, platforma.courses.name, platforma.courses.description
-FROM platforma.courses LEFT OUTER JOIN dim_course on (platforma.courses.id = dim_course.id)
-WHERE platforma.courses.skey is NULL
-
--- find modified
-SELECT dim_course.skey, platforma.courses.id, platforma.courses.name, platforma.courses.description
-INTO #scd2
-FROM platforma.courses INNER JOIN dim_course on (platforma.courses.id = dim_course.id) and (platforma.courses.name != dim_course.name or platforma.courses.description != dim_course.description)
-WHERE dim_course.end_date = "9999-12-31"
-
--- update table
-UPDATE dim_course
-SET end_date=NOW()
-FROM #scd2
-WHERE #scd2.skey = dim_course.skey
-
--- add updated rows
-INSERT INTO dim_course(id, name, description)
-SELECT id, name, description
-FROM #scd2
-
-END; $$
-
-
  -- DDL for dim_admins
 CREATE TABLE dim_admins(
   skey serial primary key,
@@ -76,6 +35,47 @@ WHERE #scd2.skey = dim_admins.skey
 -- add updated rows
 INSERT INTO dim_admins(userid, firstname, lastname)
 SELECT userid, firstname, lastname
+FROM #scd2
+
+END; $$
+
+
+ -- DDL for dim_course
+CREATE TABLE dim_course(
+  skey serial primary key,
+  id int4 NOT NULL,
+  name varchar NOT NULL,
+  description varchar,
+  start_date timestamp NOT NULL default(NOW()),
+  end_date timestamp default("9999-12-31")
+);
+
+ -- Procedure for dim_course
+CREATE OR REPLACE PROCEDURE sp_performETL_Course
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+
+INSERT INTO dim_course
+SELECT platforma.courses.id, platforma.courses.name, platforma.courses.description
+FROM platforma.courses LEFT OUTER JOIN dim_course on (platforma.courses.id = dim_course.id)
+WHERE platforma.courses.skey is NULL
+
+-- find modified
+SELECT dim_course.skey, platforma.courses.id, platforma.courses.name, platforma.courses.description
+INTO #scd2
+FROM platforma.courses INNER JOIN dim_course on (platforma.courses.id = dim_course.id) and (platforma.courses.name != dim_course.name or platforma.courses.description != dim_course.description)
+WHERE dim_course.end_date = "9999-12-31"
+
+-- update table
+UPDATE dim_course
+SET end_date=NOW()
+FROM #scd2
+WHERE #scd2.skey = dim_course.skey
+
+-- add updated rows
+INSERT INTO dim_course(id, name, description)
+SELECT id, name, description
 FROM #scd2
 
 END; $$
@@ -121,4 +121,4 @@ FROM #scd2
 
 END; $$
 
-CALL sp_performETL_CourseCALL sp_performETL_AdminsCALL sp_performETL_Students
+CALL sp_performETL_AdminsCALL sp_performETL_CourseCALL sp_performETL_Students
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="Generating_dimension_tables_from_csv", # Replace with your own username
     version="1.0",
     author="David Galevski, Jovan Velkovski",
-    author_email="davidgalevski@gmail.com, velkovskijovan2@gmail.com",
+    author_email="davidgalevski@gmail.com, jovanvelkovski2@gmail.com",
     description="Given a csv of the desired dimensions, the package connects to the database and creates a Warehouse"
                 " with dimension tables.",
     url="https://github.com/becutandavid/Generating-SQL-code-for-ETL",
